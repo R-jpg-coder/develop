65d930b6e1a64ea8c41b8cced4748af9d5e1c001
R-jpg-coder <72785465+R-jpg-coder@users.noreply.github.com>
2024-07-02 23:57:17 +0600

    Add files via upload

---
+++ views.py
@@ -0,0 +1,25 @@
+
+
+# Create your views here.
+# students/views.py
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Student
+from .forms import StudentForm
+
+def edit_student(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+    else:
+        form = StudentForm(instance=student)
+    return render(request, 'students/edit_student.html', {'form': form})
+
+def delete_student(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == 'POST':
+        student.delete()
+        return redirect('student_list')
+    return render(request, 'students/delete_student.html', {'student': student})
