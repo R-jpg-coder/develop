eed1c737757ee93db7b063e9e414048c79a48c1b
R-jpg-coder <72785465+R-jpg-coder@users.noreply.github.com>
2024-07-13 11:25:02 +0600

    Add files via upload

---
+++ polls7/forms.py
@@ -0,0 +1,12 @@
+def perform_create(self, serializer):
+    queryset = SignupRequest.objects.filter(user=self.request.user)
+    if queryset.exists():
+        raise ValidationError('You have already signed up')
+    serializer.save(user=self.request.user)
+    
+    
+    def perform_create(self, serializer):
+    queryset = SignupRequest.objects.filter(user=self.request.user)
+    if queryset.exists():
+        raise ValidationError('You have already signed up')
+    serializer.save(user=self.request.user)
